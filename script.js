ea70c79fb844f46743e288aca83bd5494cdee735
Alfredo Sepulveda <sepulveda.sastre@gmail.com>
2020-10-19 21:34:24 -0500

    agregar caracola js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('Hola Mundo!')
+function show_caracola() {
+
+  var random1 = Math.floor((Math.random()*4+1));
+
+if (random1==1) {
+  random1 = 'Sí.' ;
+  }
+  else { if (random1==2) {
+  random1 = 'No.' ;
+  }
+  else { if (random1==2) {
+  random1 = 'Quizá.' ;
+  }
+  else { if (random1==1) {
+  random1 = 'Nada.' ;
+  }
+  else { random1 = 'Intenta preguntar de nuevo.'; 
+  }
+
+  }
+
+  }
+
+  }
+  document.getElementById('caracola').innerHTML = '<h1>' + random1 + '</h1>'
+}
+
+
